cc9025c53bb2a8cc5627c2e9c6a8168a8c179846
Mustapha KEBRI <kazeg@oceaneconsulting.com>
2016-03-24 11:46:53 +0100

    add rule implementation: 10.9.1

--- rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule100901Test.java
+++ rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule100901Test.java
@@ -19,8 +19,10 @@
  */
 package org.tanaguru.rules.rgaa30;
 
-import org.tanaguru.entity.audit.TestSolution;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.ProcessResult;
+import org.tanaguru.entity.audit.TestSolution;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG;
 import org.tanaguru.rules.rgaa30.test.Rgaa30RuleImplementationTestCase;
 
 /**
@@ -46,7 +48,7 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa30.Test.10.9.1-1Passed-01");
+        addWebResource("Rgaa30.Test.10.9.1-1Passed-01");
 //        addWebResource("Rgaa30.Test.10.9.1-2Failed-01");
         addWebResource("Rgaa30.Test.10.9.1-3NMI-01");
 //        addWebResource("Rgaa30.Test.10.9.1-4NA-01");
@@ -57,7 +59,7 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
         //----------------------------------------------------------------------
         //------------------------------1Passed-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa30.Test.10.9.1-1Passed-01"), 1);
+        checkResultIsPassed(processPageTest("Rgaa30.Test.10.9.1-1Passed-01"), 0);
 
         //----------------------------------------------------------------------
         //------------------------------2Failed-01------------------------------
@@ -76,15 +78,14 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
         ProcessResult processResult = processPageTest("Rgaa30.Test.10.9.1-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+//        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
+          checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG,
+                "p",
+                1);
 
 
         //----------------------------------------------------------------------
@@ -99,8 +100,8 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
         // The consolidate method can be removed when real implementation is done.
         // The assertions are automatically tested regarding the file names by 
         // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa30.Test.10.9.1-3NMI-01").getValue());
+//        assertEquals(TestSolution.NOT_TESTED,
+//                consolidate("Rgaa30.Test.10.9.1-3NMI-01").getValue());
     }
 
 }
