9be775e5769b815a65a49a21485c86ce2dec298a
wellcom <kazeg@oceaneconsulting.com>
2016-05-31 16:31:32 +0200

    Fix unit tests for rule 10.9.1

--- rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule100901Test.java
+++ rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule100901Test.java
@@ -22,7 +22,8 @@ package org.tanaguru.rules.rgaa30;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.ProcessResult;
 import org.tanaguru.entity.audit.TestSolution;
-import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG;
+import org.tanaguru.rules.keystore.HtmlElementStore;
+import org.tanaguru.rules.keystore.RemarkMessageStore;
 import org.tanaguru.rules.rgaa30.test.Rgaa30RuleImplementationTestCase;
 
 /**
@@ -49,8 +50,9 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
     @Override
     protected void setUpWebResourceMap() {
         addWebResource("Rgaa30.Test.10.9.1-1Passed-01");
-//        addWebResource("Rgaa30.Test.10.9.1-2Failed-01");
-        addWebResource("Rgaa30.Test.10.9.1-3NMI-01");
+        addWebResource("Rgaa30.Test.10.9.1-2Failed-01");
+        addWebResource("Rgaa30.Test.10.9.1-2Failed-02");
+ //       addWebResource("Rgaa30.Test.10.9.1-3NMI-01");
 //        addWebResource("Rgaa30.Test.10.9.1-4NA-01");
     }
 
@@ -64,28 +66,40 @@ public class Rgaa30Rule100901Test extends Rgaa30RuleImplementationTestCase {
         //----------------------------------------------------------------------
         //------------------------------2Failed-01------------------------------
         //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa30.Test.10.9.1-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
+        ProcessResult processResult = processPageTest("Rgaa30.Test.10.9.1-2Failed-01");
+        checkResultIsFailed(processResult, 1, 1);
 //        checkRemarkIsPresent(
 //                processResult,
 //                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
+//                CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG,
+//                "h1",
+//                1,
+//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-02------------------------------
+        //----------------------------------------------------------------------
+              processResult = processPageTest("Rgaa30.Test.10.9.1-2Failed-02");
+         checkResultIsFailed(processResult, 1, 1);
+//        checkRemarkIsPresent(
+//                processResult,
+//                TestSolution.FAILED,
+//                RemarkMessageStore.CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG,
+//                HtmlElementStore.P_ELEMENT,
 //                1,
 //                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
 
         //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa30.Test.10.9.1-3NMI-01");
+//        ProcessResult processResult = processPageTest("Rgaa30.Test.10.9.1-3NMI-01");
 //        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-          checkResultIsPreQualified(processResult, 1, 1);
-        checkRemarkIsPresent(
-                processResult,
-                TestSolution.NEED_MORE_INFO,
-                CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG,
-                "p",
-                1);
+//          checkResultIsPreQualified(processResult, 1, 1);
+//        checkRemarkIsPresent(
+//                processResult,
+//                TestSolution.NEED_MORE_INFO,
+//                CHECK_IF_USER_HAVE_MECHANISM_TO_DELETE_JUSTIFY_TEXT_ALIGN_MSG,
+//                "p",
+//                1);
 
 
         //----------------------------------------------------------------------
