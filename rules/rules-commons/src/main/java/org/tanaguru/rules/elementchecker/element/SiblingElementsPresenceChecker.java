fb877bcecbc186a01b2da07045a5c1a23639c630
Imen <ijdidi@oceaneconsulting.com>
2017-05-03 17:10:46 +0200

    Fix rule 4.1.1

---
+++ rules/rules-commons/src/main/java/org/tanaguru/rules/elementchecker/element/SiblingElementsPresenceChecker.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tanaguru.rules.elementchecker.element;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.tanaguru.entity.audit.TestSolution;
+import org.tanaguru.processor.SSPHandler;
+import org.tanaguru.ruleimplementation.TestSolutionHandler;
+import org.tanaguru.rules.elementchecker.ElementCheckerImpl;
+
+/**
+ *
+ * @author tanaguru
+ */
+public class SiblingElementsPresenceChecker extends ElementCheckerImpl {
+
+    /**
+     * The minimum number of children required. Default is 8
+     */
+    private int minimumNumberOfChildRequired = 1;
+
+    /**
+     * The child elements to search
+     */
+    private final Collection<String> childTextualElementNames = new ArrayList<>();
+
+    /**
+     * Constructor.
+     *
+     * @param siblingTextualElementName
+     * @param detectedSolutionPair
+     * @param notDetectedSolutionPair
+     * @param eeAttributeNameList
+     */
+    public SiblingElementsPresenceChecker(
+            List<String> siblingTextualElementName,
+            Pair<TestSolution, String> detectedSolutionPair,
+            Pair<TestSolution, String> notDetectedSolutionPair,
+            String... eeAttributeNameList) {
+        super(detectedSolutionPair, notDetectedSolutionPair, eeAttributeNameList);
+        this.childTextualElementNames.addAll(siblingTextualElementName);
+    }
+
+    @Override
+    public void doCheck(
+            SSPHandler sspHandler,
+            Elements elements,
+            TestSolutionHandler testSolutionHandler) {
+        for (String elementToTest : childTextualElementNames) {
+            checkChildElementPresence(elementToTest, elements, testSolutionHandler);
+        }
+    }
+
+    /**
+     * This methods checks whether elements have a child element of a given
+     * type.
+     *
+     * @param elementToTest
+     * @param elements
+     * @param testSolutionHandler
+     */
+    private void checkChildElementPresence(
+            String elementToTest,
+            //  Element currentTestedElement,
+            Elements elements,
+            TestSolutionHandler testSolutionHandler) {
+        // parent holds the parent element of our sibling elements
+        Element parent = null;
+        Elements siblingElements = null;
+
+        if (elements.isEmpty()) {
+            testSolutionHandler.addTestSolution(TestSolution.NOT_APPLICABLE);
+            return;
+        }
+
+        TestSolution testSolution = TestSolution.PASSED;
+
+        for (Element el : elements) {
+
+            parent = el.parent();
+            siblingElements = parent.children();
+
+            if (parent == null) { //if no parent witch means no body tag neither html tag ==> test not applicable  incorrect html code !
+                testSolutionHandler.addTestSolution(TestSolution.NOT_APPLICABLE);
+                return;
+            } else if (siblingElements.size() <= 1) {
+                testSolution = setTestSolution(testSolution, TestSolution.FAILED);
+                addSourceCodeRemark(TestSolution.FAILED, el, getFailureMsgCode());
+            } else {
+
+                for (Element sEl : siblingElements) {
+                    if (sEl.getElementsByTag(elementToTest).size() >= 1) {
+                        testSolution = setTestSolution(testSolution, TestSolution.NEED_MORE_INFO);
+                        addSourceCodeRemark(TestSolution.NEED_MORE_INFO, el, getSuccessMsgCode());
+
+                    }
+
+                }
+
+//                testSolution = setTestSolution(testSolution, getFailureSolution());
+//                addSourceCodeRemark(getFailureSolution(), el, getFailureMsgCode());
+//            }
+//            
+//            
+//
+//            if (el.getElementsByTag(elementToTest).size() >= minimumNumberOfChildRequired) {
+//                elements.indexOf(el);
+//                testSolution = setTestSolution(testSolution, getSuccessSolution());
+//                addSourceCodeRemark(getSuccessSolution(), el, getSuccessMsgCode());
+//
+//            } else {
+//
+//                testSolution = setTestSolution(testSolution, getFailureSolution());
+//                addSourceCodeRemark(getFailureSolution(), el, getFailureMsgCode());
+//
+            }
+        }
+
+        testSolutionHandler.addTestSolution(testSolution);
+    }
+
+}
