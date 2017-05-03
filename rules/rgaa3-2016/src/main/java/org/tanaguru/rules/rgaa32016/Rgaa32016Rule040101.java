b040f80166dfd31b2ee0fb70222d6f87f59b514c
Imen <ijdidi@oceaneconsulting.com>
2017-05-03 17:10:46 +0200

    Fix rule 4.1.1

--- rules/rgaa3-2016/src/main/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule040101.java
+++ rules/rgaa3-2016/src/main/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule040101.java
@@ -19,11 +19,17 @@
  */
 package org.tanaguru.rules.rgaa32016;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.TestSolution;
-import org.tanaguru.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.tanaguru.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.tanaguru.rules.elementchecker.element.SiblingElementsPresenceChecker;
 import org.tanaguru.rules.elementselector.SimpleElementSelector;
+import static org.tanaguru.rules.keystore.AttributeStore.SRC_ATTR;
 import static org.tanaguru.rules.keystore.CssLikeQueryStore.MEDIA_AUDIO_CSS_LIKE_QUERY;
-import static org.tanaguru.rules.keystore.RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG;
+import static org.tanaguru.rules.keystore.CssLikeQueryStore.MEDIA_ONLY_VIDEO_TRANS_CSS_LIKE_QUERY;
+import static org.tanaguru.rules.rgaa32016.Rgaa32016Rule040101.textualElementsTag;
 
 /**
  * Implementation of the rule 4.1.1 of the referential Rgaa 3-2016.
@@ -36,24 +42,45 @@ import static org.tanaguru.rules.keystore.RemarkMessageStore.MANUAL_CHECK_ON_ELE
  * <a href="http://references.modernisation.gouv.fr/referentiel-technique-0#test-4-1-1">
  * 4.1.1 rule specification</a>
  */
-public class Rgaa32016Rule040101 extends AbstractDetectionPageRuleImplementation {
+public class Rgaa32016Rule040101 extends AbstractPageRuleWithSelectorAndCheckerImplementation {
 
-    /**
-     * Default constructor
-     */
+    // AbstractDetectionPageRuleImplementation
+//    /**
+//     * Default constructor
+//     */
+//    public Rgaa32016Rule040101() {
+//        super(
+//                new SimpleElementSelector(MEDIA_AUDIO_CSS_LIKE_QUERY),
+//                // solution when at least one element is found
+//                TestSolution.NEED_MORE_INFO,
+//                // solution when no element is found
+//                TestSolution.NOT_APPLICABLE,
+//                // manual check message
+//                MANUAL_CHECK_ON_ELEMENTS_MSG,
+//                null
+//                // evidence element
+//                
+//        );
+//    }
+    public static List<String> textualElementsTag = new ArrayList<String>() {
+        {
+            add("p");
+            add("h1");
+            add("h2");
+        }
+    };
+
     public Rgaa32016Rule040101() {
-        super(
-                new SimpleElementSelector(MEDIA_AUDIO_CSS_LIKE_QUERY),
-                // solution when at least one element is found
-                TestSolution.NEED_MORE_INFO,
-                // solution when no element is found
-                TestSolution.NOT_APPLICABLE,
-                // manual check message
-                MANUAL_CHECK_ON_ELEMENTS_MSG,
-                null
-                // evidence element
-                
-        );
+        super(new SimpleElementSelector(MEDIA_AUDIO_CSS_LIKE_QUERY),
+                // new AttributeOnChildElementPresenceChecker(
+                //"kind",
+                // the set is not empty
+                new SiblingElementsPresenceChecker(
+                        textualElementsTag,
+                        new ImmutablePair(TestSolution.NEED_MORE_INFO, "msg"),
+                        new ImmutablePair(TestSolution.NOT_APPLICABLE, ""),
+                        SRC_ATTR));
+
     }
 
 }
