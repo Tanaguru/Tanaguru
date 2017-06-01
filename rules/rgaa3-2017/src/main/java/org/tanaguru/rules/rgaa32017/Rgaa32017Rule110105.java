66a3e6e19a503599ce2aad107f9849c9397ce9a5
Mustapha KEBRI <well_com@live.fr>
2017-06-01 19:19:19 +0200

    Add implementation rule Rgaa32017Rule110105

---
+++ rules/rgaa3-2017/src/main/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule110105.java
@@ -0,0 +1,45 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2017  Tanaguru.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: tanaguru AT tanaguru DOT org
+ */
+package org.tanaguru.rules.rgaa32017;
+
+import org.tanaguru.ruleimplementation.AbstractNotTestedRuleImplementation;
+
+/**
+ *
+ * @author tanaguru
+ */
+
+/**
+ * Implementation of the rule 11.01.05 of the referential Rgaa 3-2017.
+ *
+ * For more details about the implementation, refer to <a href="http://tanaguru-rules-rgaa3.readthedocs.org/en/latest/Rule-11-01-05">the rule 11.01.05 design page.</a>
+ * @see <a href="http://references.modernisation.gouv.fr/referentiel-technique-0#test-11-10-10"> 11.01.05 rule specification</a>
+ */
+
+public class Rgaa32017Rule110105 extends AbstractNotTestedRuleImplementation {
+
+    /**
+     * Default constructor
+     */
+    public Rgaa32017Rule110105 () {
+        super();
+    }
+
+}
