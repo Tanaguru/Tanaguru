a131e8c20995f31927876a1858309d11058079e4
Mustapha KEBRI <well_com@live.fr>
2017-06-01 19:19:19 +0200

    Add implementation rule Rgaa32017Rule010310

---
+++ rules/rgaa3-2017/src/main/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule010310.java
@@ -0,0 +1,90 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2017  Tanaguru.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: tanaguru AT tanaguru DOT org
+ */
+package org.tanaguru.rules.rgaa32017;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.tanaguru.entity.audit.TestSolution;
+import org.tanaguru.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.tanaguru.rules.elementchecker.ElementChecker;
+import org.tanaguru.rules.elementchecker.element.ElementPresenceChecker;
+import org.tanaguru.rules.elementselector.SimpleElementSelector;
+import static org.tanaguru.rules.keystore.CssLikeQueryStore.CANVAS_NOT_IN_LINK_WITH_NOT_EMPTY_CONTENT_CSS_LIKE_QUERY;
+import static org.tanaguru.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.tanaguru.rules.keystore.MarkerStore.DECORATIVE_IMAGE_MARKER;
+import static org.tanaguru.rules.keystore.MarkerStore.INFORMATIVE_IMAGE_MARKER;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_ALT_PERTINENCE_OF_INFORMATIVE_IMG_MSG;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE_AND_ALT_PERTINENCE_MSG;
+
+/**
+ * Implementation of the rule 1.3.10 of the referential Rgaa 3-2017.
+ *
+ * For more details about the implementation, refer to
+ * <a href="http://tanaguru-rules-rgaa3.readthedocs.org/en/latest/Rule-1-3-10">the
+ * rule 1.3.10 design page.</a>
+ *
+ * @see
+ * <a href="http://references.modernisation.gouv.fr/referentiel-technique-0#test-1-3-10">
+ * 1.3.9 rule specification</a>
+ */
+public class Rgaa32017Rule010310 extends AbstractMarkerPageRuleImplementation {
+
+    /**
+     * Default constructor
+     */
+    public Rgaa32017Rule010310() {
+        super(
+                // the informative images are part of the scope
+                INFORMATIVE_IMAGE_MARKER,
+                // the decorative images are not part of the scope
+                DECORATIVE_IMAGE_MARKER);
+
+        setElementSelector(new SimpleElementSelector(CANVAS_NOT_IN_LINK_WITH_NOT_EMPTY_CONTENT_CSS_LIKE_QUERY));
+        setMarkerElementChecker(getMarkerElementChecker());
+        setRegularElementChecker(getLocalRegularElementChecker());
+    }
+
+    /**
+     *
+     * @return the checker user for marked elements
+     */
+    private ElementChecker getMarkerElementChecker() {
+        ElementChecker ec = new ElementPresenceChecker(
+                new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_ALT_PERTINENCE_OF_INFORMATIVE_IMG_MSG),
+                new ImmutablePair(TestSolution.NOT_APPLICABLE, ""),
+                // evidence element
+                TEXT_ELEMENT2
+        );
+        return ec;
+    }
+    
+    /**
+     *
+     * @return the checker user for marked elements
+     */
+    private ElementChecker getLocalRegularElementChecker() {
+        ElementChecker ec = new ElementPresenceChecker(
+                new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_NATURE_OF_IMAGE_AND_ALT_PERTINENCE_MSG),
+                new ImmutablePair(TestSolution.NOT_APPLICABLE, ""),
+                // evidence element
+                TEXT_ELEMENT2
+        );
+        return ec;
+    }
+}
