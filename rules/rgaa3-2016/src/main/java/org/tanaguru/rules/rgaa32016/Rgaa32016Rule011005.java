47cee6a300dd49ec04314f9ad95f67ecaecd5ac8
Imen <ijdidi@oceaneconsulting.com>
2017-06-21 12:08:07 +0200

    Add rule 1.10.5 implementation

--- rules/rgaa3-2016/src/main/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule011005.java
+++ rules/rgaa3-2016/src/main/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule011005.java
@@ -19,7 +19,12 @@
  */
 package org.tanaguru.rules.rgaa32016;
 
-import org.tanaguru.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.tanaguru.entity.audit.TestSolution;
+import org.tanaguru.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.tanaguru.rules.elementchecker.element.ElementPresenceChecker;
+import org.tanaguru.rules.elementselector.SimpleElementSelector;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_CANVAS_LEGD_ARIA_MSG;
 
 /**
  * Implementation of the rule 1.10.5 of the referential Rgaa 3-2016.
@@ -28,13 +33,17 @@ import org.tanaguru.ruleimplementation.AbstractNotTestedRuleImplementation;
  * @see <a href="http://references.modernisation.gouv.fr/referentiel-technique-0#test-1-10-5"> 1.10.5 rule specification</a>
  */
 
-public class Rgaa32016Rule011005 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule011005 extends AbstractPageRuleWithSelectorAndCheckerImplementation {
 
     /**
      * Default constructor
      */
     public Rgaa32016Rule011005 () {
-        super();
+        super(new SimpleElementSelector("canvas"),
+                new ElementPresenceChecker(
+                new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_CANVAS_LEGD_ARIA_MSG),
+                new ImmutablePair(TestSolution.NOT_APPLICABLE, "")
+                ));
     }
 
 }
