3127978d3ab12eb7af2cc37a4c27783029f63b17
Imen <ijdidi@oceaneconsulting.com>
2017-06-21 11:37:49 +0200

    Add rule 1.10.1 implementation

--- rules/rgaa3-2016/src/test/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule011001Test.java
+++ rules/rgaa3-2016/src/test/java/org/tanaguru/rules/rgaa32016/Rgaa32016Rule011001Test.java
@@ -19,9 +19,7 @@
  */
 package org.tanaguru.rules.rgaa32016;
 
-import org.tanaguru.entity.audit.TestSolution;
 import org.tanaguru.entity.audit.ProcessResult;
-import org.tanaguru.rules.keystore.RemarkMessageStore;
 import org.tanaguru.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
 
 /**
@@ -29,58 +27,34 @@ import org.tanaguru.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
  *
  * @author
  */
-public class Rgaa32016Rule011001Test extends Rgaa32016RuleImplementationTestCase {
+public class Rgaa32016Rule011001Test extends Rgaa32016RuleImplementationTestCase{
 
-    /**
-     * Default constructor
-     * @param testName
-     */
-    public Rgaa32016Rule011001Test (String testName){
+    public Rgaa32016Rule011001Test(String testName) {
         super(testName);
     }
 
     @Override
     protected void setUpRuleImplementationClassName() {
-        setRuleImplementationClassName(
-                "org.tanaguru.rules.rgaa32016.Rgaa32016Rule011001");
+        setRuleImplementationClassName("org.tanaguru.rules.rgaa32016.Rgaa32016Rule011001");
     }
 
     @Override
     protected void setUpWebResourceMap() {
-        addWebResource("Rgaa32016.Test.1.10.1-3NMI-01");
-        addWebResource("Rgaa32016.Test.1.10.1-3NMI-02");
+        addWebResource("Rgaa32016.Test.1.10.1-3NMI-03");
         addWebResource("Rgaa32016.Test.1.10.1-4NA-01");
     }
 
     @Override
     protected void setProcess() {
-        //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
-        //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa32016.Test.1.10.1-3NMI-01");
-        checkResultIsPreQualified(processResult, 0, 1);
-        checkRemarkIsPresent(
-                processResult, 
-                TestSolution.NEED_MORE_INFO, 
-                RemarkMessageStore.HTML5_DOCTYPE_DETECTED_CHECK_MANUALLY, 
-                1);
-
-
-        //----------------------------------------------------------------------
-        //------------------------------3NMI-02---------------------------------
-        //----------------------------------------------------------------------
-        processResult = processPageTest("Rgaa32016.Test.1.10.1-3NMI-02");
-        checkResultIsPreQualified(processResult, 0, 1);
-        checkRemarkIsPresent(
-                processResult, 
-                TestSolution.NEED_MORE_INFO, 
-                RemarkMessageStore.DOCTYPE_ABSENT_CHECK_HTML5, 
-                1);
-                
-        //----------------------------------------------------------------------
+        
+       ProcessResult processResult = processPageTest("Rgaa32016.Test.1.10.1-3NMI-03");
+        checkResultIsPreQualified(processResult, 2, 2);
+        
         //------------------------------4NA-01------------------------------
         //----------------------------------------------------------------------
         checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.1.10.1-4NA-01"));
-    }
 
+    }
+    
 }
