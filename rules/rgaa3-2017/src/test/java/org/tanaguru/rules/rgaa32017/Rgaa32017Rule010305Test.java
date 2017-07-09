f836e99b439db81fe58b00846e395246e0cb678e
Mustapha KEBRI <well_com@live.fr>
2017-07-09 19:19:19 +0200

    Add implementation rule Rgaa32017Rule010305Test

---
+++ rules/rgaa3-2017/src/test/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule010305Test.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tanaguru.rules.rgaa32017;
+
+import org.tanaguru.entity.audit.ProcessResult;
+import org.tanaguru.rules.rgaa32017.test.Rgaa32017RuleImplementationTestCase;
+
+/**
+ *
+ * @author tanaguru
+ */
+public class Rgaa32017Rule010305Test extends Rgaa32017RuleImplementationTestCase{
+
+    public Rgaa32017Rule010305Test(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUpRuleImplementationClassName() {
+        setRuleImplementationClassName("org.tanaguru.rules.rgaa32017.Rgaa32017Rule010305");
+    }
+
+    @Override
+    protected void setUpWebResourceMap() {
+        addWebResource("Rgaa32017.Test.1.3.5-3NMI-01");
+        addWebResource("Rgaa32017.Test.1.3.5-4NA-01");
+    }
+
+    @Override
+    protected void setProcess() {
+        //------------------------------3NMI-01---------------------------------
+        
+       ProcessResult processResult = processPageTest("Rgaa32017.Test.1.3.5-3NMI-01");
+        checkResultIsPreQualified(processResult, 2, 2);
+        
+        //------------------------------4NA-01------------------------------
+        //----------------------------------------------------------------------
+        checkResultIsNotApplicable(processPageTest("Rgaa32017.Test.1.3.5-4NA-01"));
+
+    }
+    
+}
