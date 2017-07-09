e3b1b5102ab485e47870d2167353c19a15be0845
Mustapha KEBRI <well_com@live.fr>
2017-07-09 19:19:19 +0200

    Add implementation rule Rgaa32017Rule030303Test

---
+++ rules/rgaa3-2017/src/test/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule030303Test.java
@@ -0,0 +1,79 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2017  Tanaguru.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: tanaguru AT tanaguru DOT org
+ */
+package org.tanaguru.rules.rgaa32017;
+
+import org.tanaguru.entity.audit.TestSolution;
+import org.tanaguru.rules.rgaa32017.test.Rgaa32017RuleImplementationTestCase;
+
+/**
+ * Unit test class for the implementation of the rule 3-3-3 of the referential Rgaa 3-2017.
+ *
+ * @author jkowalczyk
+ */
+public class Rgaa32017Rule030303Test extends Rgaa32017RuleImplementationTestCase {
+
+    /**
+     * Default constructor
+     * @param testName
+     */
+    public Rgaa32017Rule030303Test (String testName){
+        super(testName);
+    }
+
+    @Override
+    protected void setUpRuleImplementationClassName() {
+        setRuleImplementationClassName("org.tanaguru.rules.rgaa32017.Rgaa32017Rule030303");
+    }
+
+    @Override
+    protected void setUpWebResourceMap() {
+//        addWebResource("Rgaa32017.Test.03.03.03-1Passed-01");
+//        addWebResource("Rgaa32017.Test.03.03.03-2Failed-01");
+        addWebResource("Rgaa32017.Test.03.03.03-3NMI-01");
+//        addWebResource("Rgaa32017.Test.03.03.03-4NA-01");
+
+    }
+
+    @Override
+    protected void setProcess() {
+//        assertEquals(TestSolution.PASSED,
+//                processPageTest("Rgaa32017.Test.03.03.03-1Passed-01").getValue());
+//        assertEquals(TestSolution.FAILED,
+//                processPageTest("Rgaa32017.Test.03.03.03-2Failed-01").getValue());
+        assertEquals(TestSolution.NOT_TESTED,
+                processPageTest("Rgaa32017.Test.03.03.03-3NMI-01").getValue());
+//        assertEquals(TestSolution.NOT_APPLICABLE,
+//                processPageTest("Rgaa32017.Test.03.03.03-4NA-01").getValue());
+
+    }
+
+    @Override
+    protected void setConsolidate() {
+//        assertEquals(TestSolution.PASSED,
+//                consolidate("Rgaa32017.Test.03.03.03-1Passed-01").getValue());
+//        assertEquals(TestSolution.FAILED,
+//                consolidate("Rgaa32017.Test.03.03.03-2Failed-01").getValue());
+        assertEquals(TestSolution.NOT_TESTED,
+                consolidate("Rgaa32017.Test.03.03.03-3NMI-01").getValue());
+//        assertEquals(TestSolution.NOT_APPLICABLE,
+//                consolidate("Rgaa32017.Test.03.03.03-4NA-01").getValue());
+    }
+
+}
