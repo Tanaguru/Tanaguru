d57ac6eaaf29cd145ef44dd93f8a86970e9f5d1b
Mustapha KEBRI <well_com@live.fr>
2017-07-13 19:19:19 +0200

    Add implementation rule Rgaa32017Rule111101Test

---
+++ rules/rgaa3-2017/src/test/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule111101Test.java
@@ -0,0 +1,77 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2017  Tanaguru.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: tanaguru AT tanaguru DOT org
+ */
+package org.tanaguru.rules.rgaa32017;
+
+import org.tanaguru.entity.audit.ProcessResult;
+import org.tanaguru.entity.audit.TestSolution;
+import org.tanaguru.rules.rgaa32017.test.Rgaa32017RuleImplementationTestCase;
+import org.tanaguru.rules.keystore.HtmlElementStore;
+import org.tanaguru.rules.keystore.RemarkMessageStore;
+
+/**
+ * Unit test class for the implementation of the rule 11-11-1 of the referential Rgaa 3-2017.
+ *
+ * @author jkowalczyk
+ */
+public class Rgaa32017Rule111101Test extends Rgaa32017RuleImplementationTestCase {
+
+    /**
+     * Default constructor
+     * @param testName
+     */
+    public Rgaa32017Rule111101Test (String testName){
+        super(testName);
+    }
+
+    @Override
+    protected void setUpRuleImplementationClassName() {
+        setRuleImplementationClassName("org.tanaguru.rules.rgaa32017.Rgaa32017Rule111101");
+    }
+
+    @Override
+    protected void setUpWebResourceMap() {
+        addWebResource("Rgaa32017.Test.11.11.01-3NMI-01");
+        addWebResource("Rgaa32017.Test.11.11.01-4NA-01");
+
+    }
+
+    @Override
+    protected void setProcess() {
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-01---------------------------------
+        //----------------------------------------------------------------------
+        ProcessResult processResult = processPageTest("Rgaa32017.Test.11.11.01-3NMI-01");
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG,
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+               
+        
+        //----------------------------------------------------------------------
+        //------------------------------4NA-01----------------------------------
+        //----------------------------------------------------------------------
+        checkResultIsNotApplicable(processPageTest("Rgaa32017.Test.11.11.01-4NA-01"));
+
+    }
+
+}
