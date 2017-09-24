a5b9af767a2a68c76a996fe7dea39e078d4c264f
Mustapha KEBRI <well_com@live.fr>
2017-09-24 23:25:34 +0200

    Add Accedeweb interfaces

---
+++ engine/tanaguru-api/src/main/java/org/tanaguru/entity/reference/Accedeweb.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tanaguru.entity.reference;
+
+import org.tanaguru.sdk.entity.Entity;
+
+/**
+ *
+ * @author mkebri
+ */
+public interface Accedeweb extends Entity {
+
+    /**
+     *
+     * @return the code
+     */
+    String getCode();
+
+    /**
+     *
+     * @return the Description
+     */
+    String getDescription();
+
+    /**
+     *
+     * @param code the code to set
+     */
+    void setCode(String code);
+
+    /**
+     *
+     * @param criterion the criterion to set
+     */
+    void setDescription(String description);
+
+    /**
+     *
+     * @return the Description
+     */
+    String getCd_Rgaa32016();
+
+    /**
+     *
+     * @param code the code to set
+     */
+    void setCd_Rgaa32016(String Cd_Rgaa32016);
+
+    /**
+     *
+     * @return the code
+     */
+    Test getTest();
+
+    /**
+     *
+     * @return the Description
+     */
+    void setTest(Test test);
+}
