09a859c8c4d3470c03aabf4362d022d61ec13909
Mustapha KEBRI <well_com@live.fr>
2018-02-26 11:13:33 +0100

    Add Test Unit for rule 11.10.02

--- rules/rgaa3-2017/src/test/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule111002Test.java
+++ rules/rgaa3-2017/src/test/java/org/tanaguru/rules/rgaa32017/Rgaa32017Rule111002Test.java
@@ -19,8 +19,11 @@
  */
 package org.tanaguru.rules.rgaa32017;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.TestSolution;
 import org.tanaguru.entity.audit.ProcessResult;
+import org.tanaguru.rules.keystore.HtmlElementStore;
+import org.tanaguru.rules.keystore.RemarkMessageStore;
 import org.tanaguru.rules.rgaa32017.test.Rgaa32017RuleImplementationTestCase;
 
 /**
@@ -49,7 +52,9 @@ public class Rgaa32017Rule111002Test extends Rgaa32017RuleImplementationTestCase
 //        addWebResource("Rgaa32017.Test.11.10.2-1Passed-01");
 //        addWebResource("Rgaa32017.Test.11.10.2-2Failed-01");
         addWebResource("Rgaa32017.Test.11.10.2-3NMI-01");
-//        addWebResource("Rgaa32017.Test.11.10.2-4NA-01");
+        addWebResource("Rgaa32017.Test.11.10.2-3NMI-02");
+        addWebResource("Rgaa32017.Test.11.10.2-3NMI-03");
+        addWebResource("Rgaa32017.Test.11.10.2-4NA-01");
     }
 
     @Override
@@ -76,31 +81,56 @@ public class Rgaa32017Rule111002Test extends Rgaa32017RuleImplementationTestCase
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
         ProcessResult processResult = processPageTest("Rgaa32017.Test.11.10.2-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+//        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG,
+                HtmlElementStore.INPUT_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-02---------------------------------
+        //----------------------------------------------------------------------
+         processResult = processPageTest("Rgaa32017.Test.11.10.2-3NMI-02");
+//        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG,
+                HtmlElementStore.INPUT_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-02---------------------------------
+        //----------------------------------------------------------------------
+         processResult = processPageTest("Rgaa32017.Test.11.10.2-3NMI-03");
+//        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG,
+                HtmlElementStore.INPUT_ELEMENT,
+                1);
 
 
         //----------------------------------------------------------------------
         //------------------------------4NA-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa32017.Test.11.10.2-4NA-01"));
+        checkResultIsNotApplicable(processPageTest("Rgaa32017.Test.11.10.2-4NA-01"));
     }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa32017.Test.11.10.2-3NMI-01").getValue());
-    }
+//
+//    @Override
+//    protected void setConsolidate() {
+//
+//        // The consolidate method can be removed when real implementation is done.
+//        // The assertions are automatically tested regarding the file names by 
+//        // the abstract parent class
+//        assertEquals(TestSolution.NOT_TESTED,
+//                consolidate("Rgaa32017.Test.11.10.2-3NMI-01").getValue());
+//    }
 
 }
