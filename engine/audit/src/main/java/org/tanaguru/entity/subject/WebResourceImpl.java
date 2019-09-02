197748ea91bb30c7f34105a7288cda69b33c1a5b
Raphaël Charre <rcharre@oceaneconsulting.com>
2019-09-02 10:44:21 +0200

    fix annotation column type definition for snapshot

--- engine/audit/src/main/java/org/tanaguru/entity/subject/WebResourceImpl.java
+++ engine/audit/src/main/java/org/tanaguru/entity/subject/WebResourceImpl.java
@@ -66,7 +66,7 @@ public abstract class WebResourceImpl implements WebResource, Serializable {
     private Set<ProcessResultImpl> processResultSet;
     @Column(name = "Rank", nullable = false)
     private int rank = 0;
-    @Column(name = "Snapshot", columnDefinition = "BLOB")
+    @Column(name = "Snapshot", columnDefinition = "mediumblob")
     private byte[] snapshot;
 
     public WebResourceImpl() {
