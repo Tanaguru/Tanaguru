b8c53cb5d983445d79469d947228edb069620e93
Emilie DA CONCEICAO <edaconceicao@oceaneconsulting.com>
2019-09-02 16:36:42 +0200

    [RGAA4-2019] Add Rule 01.01.02
    Add Rule 01.01.02
    Not available currently

--- rules/rgaa4-2019/src/test/java/org/tanaguru/rules/rgaa42019/Rgaa42019Rule010102Test.java
+++ rules/rgaa4-2019/src/test/java/org/tanaguru/rules/rgaa42019/Rgaa42019Rule010102Test.java
@@ -20,8 +20,24 @@
 package org.tanaguru.rules.rgaa42019;
 
 import org.tanaguru.entity.audit.TestSolution;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.ProcessResult;
 import org.tanaguru.rules.rgaa42019.test.Rgaa42019RuleImplementationTestCase;
+
+import static org.tanaguru.rules.keystore.HtmlElementStore.AREA_ELEMENT;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.TEXT_ALTERNATIVE_MISSING;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE_WITHOUT_TEXT_ALTERNATIVE;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.INVALID_TEXT_ALTERNATIVE_MSG;
+import static org.tanaguru.rules.keystore.MarkerStore.DECORATIVE_IMAGE_MARKER;
+import static org.tanaguru.rules.keystore.MarkerStore.INFORMATIVE_IMAGE_MARKER;
+import static org.tanaguru.rules.keystore.AttributeStore.ALT_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.ARIA_LABELLEDBY_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.ARIA_LABEL_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.HREF_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.SRC_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.TITLE_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.ABSENT_ATTRIBUTE_VALUE;
 
 /**
  * Unit test class for the implementation of the rule 1-1-2 of the referential Rgaa 4-2019.
@@ -45,10 +61,26 @@ public class Rgaa42019Rule010102Test extends Rgaa42019RuleImplementationTestCase
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa4-2019.Test.1.1.2-1Passed-01");
-//        addWebResource("Rgaa4-2019.Test.1.1.2-2Failed-01");
-        addWebResource("Rgaa4-2019.Test.1.1.2-3NMI-01");
-//        addWebResource("Rgaa4-2019.Test.1.1.2-4NA-01");
+        addWebResource("Rgaa42019.Test.01.01.02-1Passed-01",
+                createParameter("Rules", INFORMATIVE_IMAGE_MARKER, "id-informative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-1Passed-02",
+                createParameter("Rules", INFORMATIVE_IMAGE_MARKER, "id-informative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-2Failed-01",
+                createParameter("Rules", INFORMATIVE_IMAGE_MARKER, "id-informative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-2Failed-02",
+                createParameter("Rules", INFORMATIVE_IMAGE_MARKER, "id-informative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-2Failed-03",
+                createParameter("Rules", INFORMATIVE_IMAGE_MARKER, "id-informative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-2Failed-04");
+        addWebResource("Rgaa42019.Test.01.01.02-2Failed-05");
+        addWebResource("Rgaa42019.Test.01.01.02-3NMI-01");
+        addWebResource("Rgaa42019.Test.01.01.02-3NMI-02");
+        addWebResource("Rgaa42019.Test.01.01.02-3NMI-03");
+        addWebResource("Rgaa42019.Test.01.01.02-4NA-01");
+        addWebResource("Rgaa42019.Test.01.01.02-4NA-02",
+                createParameter("Rules", DECORATIVE_IMAGE_MARKER, "id-decorative-image"));
+        addWebResource("Rgaa42019.Test.01.01.02-4NA-03",
+                createParameter("Rules", DECORATIVE_IMAGE_MARKER, "id-decorative-image"));
     }
 
     @Override
@@ -56,50 +88,199 @@ public class Rgaa42019Rule010102Test extends Rgaa42019RuleImplementationTestCase
         //----------------------------------------------------------------------
         //------------------------------1Passed-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa4-2019.Test.1.1.2-1Passed-01"), 1);
+        checkResultIsPassed(processPageTest("Rgaa42019.Test.01.01.02-1Passed-01"), 2);
+        
+        //----------------------------------------------------------------------
+        //------------------------------1Passed-02------------------------------
+        //----------------------------------------------------------------------
+        checkResultIsPassed(processPageTest("Rgaa42019.Test.01.01.02-1Passed-02"), 2);
 
         //----------------------------------------------------------------------
         //------------------------------2Failed-01------------------------------
         //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa4-2019.Test.1.1.2-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+        ProcessResult processResult = processPageTest("Rgaa42019.Test.01.01.02-2Failed-01");
+        checkResultIsFailed(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                TEXT_ALTERNATIVE_MISSING,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                TEXT_ALTERNATIVE_MISSING,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"));
+
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-02------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-2Failed-02");
+        checkResultIsFailed(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+        		new ImmutablePair(ARIA_LABELLEDBY_ATTR, "FAILED-02"));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+        		new ImmutablePair(ARIA_LABELLEDBY_ATTR, "FAILED-02"));
+
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-03------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-2Failed-03");
+        checkResultIsFailed(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(TITLE_ATTR, ""));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(TITLE_ATTR, ""));
+
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-04------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-2Failed-04");
+        checkResultIsFailed(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+        		new ImmutablePair(ARIA_LABELLEDBY_ATTR, "FAILED-04"));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+        		new ImmutablePair(ARIA_LABELLEDBY_ATTR, "FAILED-04"));
+
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-05------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-2Failed-05");
+        checkResultIsFailed(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(TITLE_ATTR, ""));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                INVALID_TEXT_ALTERNATIVE_MSG,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(TITLE_ATTR, ""));
 
         //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa4-2019.Test.1.1.2-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-3NMI-01");
+        checkResultIsPreQualified(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(ALT_ATTR, ""));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(ALT_ATTR, ""));
+
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-02---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-3NMI-02");
+        checkResultIsPreQualified(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(ARIA_LABEL_ATTR, ""));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"),
+                new ImmutablePair(ARIA_LABEL_ATTR, ""));
+
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-03---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa42019.Test.01.01.02-3NMI-03");
+        checkResultIsPreQualified(processResult, 2, 2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE_WITHOUT_TEXT_ALTERNATIVE,
+                AREA_ELEMENT,
+                1,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"));
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_NATURE_OF_IMAGE_WITHOUT_TEXT_ALTERNATIVE,
+                AREA_ELEMENT,
+                2,
+                new ImmutablePair(HREF_ATTR, "mock-link.fr"));
 
 
         //----------------------------------------------------------------------
         //------------------------------4NA-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa4-2019.Test.1.1.2-4NA-01"));
+        checkResultIsNotApplicable(processPageTest("Rgaa42019.Test.01.01.02-4NA-01"));
+
+        //----------------------------------------------------------------------
+        //------------------------------4NA-02------------------------------
+        //----------------------------------------------------------------------
+        checkResultIsNotApplicable(processPageTest("Rgaa42019.Test.01.01.02-4NA-02"));
+
+        //----------------------------------------------------------------------
+        //------------------------------4NA-03------------------------------
+        //----------------------------------------------------------------------
+        checkResultIsNotApplicable(processPageTest("Rgaa42019.Test.01.01.02-4NA-03"));
     }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa4-2019.Test.1.1.2-3NMI-01").getValue());
-}
-
 }
