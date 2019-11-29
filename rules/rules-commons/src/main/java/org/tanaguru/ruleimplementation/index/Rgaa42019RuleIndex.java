3cfbb4403d951cf7392425c00e5102cbba8ecc5a
Raphael <raphael-charre@hotmail.fr>
2019-11-29 17:29:20 +0100

    create index file for developers

---
+++ rules/rules-commons/src/main/java/org/tanaguru/ruleimplementation/index/Rgaa42019RuleIndex.java
@@ -0,0 +1,29 @@
+package org.tanaguru.ruleimplementation.index;
+
+import org.tanaguru.ruleimplementation.impl.ImageAndRoleImageHasAltAttr;
+
+public enum  Rgaa42019RuleIndex {
+    /**
+     * This enum is not used anywhere. Its only purpose is to help developers to find multi referential rules.
+     *
+     * @Author Raphaël Charre
+     */
+
+    rgaa42019_1_1_1(ImageAndRoleImageHasAltAttr.class, "RGAA42019 1.1.1");
+
+    private Class ruleClass;
+    private String rule;
+
+    Rgaa42019RuleIndex(Class ruleClass, String rule){
+        this.ruleClass = ruleClass;
+        this.rule = rule;
+    }
+
+    public Class getRuleClass() {
+        return ruleClass;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+}
